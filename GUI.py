51670efb08e664b137ee8b07b5b1937cc7431ca9
winstonf <winstonf@gmail.com>
2020-06-05 12:16:24 +0530

    Addec code to run GUI as a class

--- GUI.py
+++ GUI.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import scrolledtext 
-
-
+import threading
+import queue
+from tcp_server_v1 import TCP_Server
 class MainWindow:
         
         def __init__(self, master):
@@ -11,7 +12,7 @@ class MainWindow:
                 self.mylabel = Label(master,text="TCP Server",padx=20, pady=10).grid(row=0, column= 0)
                 self.mylabel2 = Label(master, text="Server Connected", padx=20, pady=10).grid(row = 1, column=0)
 
-                self.button1 = Button(master=, text="Send Value", command = self.buttonClick).grid(row=1, column=1)
+                self.button1 = Button(master, text="Send Value", command = self.buttonClick).grid(row=1, column=1)
 
                 self.pbar = ttk.Progressbar(orient = "horizontal", length = 350, mode="determinate", maximum="100", value = "25").grid(row = 3, column=0 , columnspan = 2) 
 
@@ -26,7 +27,9 @@ class MainWindow:
                 self.text_area.insert(INSERT, self.TBox.get())
 
 
-
+root = Tk()
+ui = MainWindow(root)
+root.mainloop()
 
 
 
